d21d516be22a73cdd00cebf728e30839a63a13b2
dburgos <david.burgos.risco@gmail.com>
2019-01-13 10:49:53 +0100

    Fix command

--- bin/ghost-purge-images.js
+++ bin/ghost-purge-images.js
@@ -2,9 +2,16 @@
 
 process.title = 'ghost-purge-images';
 
-const command = process.argv.slice(2);
+const argv = process.argv.slice(2);
 const display = require('../display.js');
 const purge = require('../purge.js');
+
+const hasCommand = argv && argv.length > 0;
+if (!hasCommand) {
+  console.log('Please provide a command, check the docs');
+  process.exit(1);
+}
+const command = argv[0];
 
 switch (command) {
   case 'purge':
@@ -14,7 +21,7 @@ switch (command) {
     display.run();
     break;
   default:
-    console.log('Please provide a command, check the docs');
+    console.log(`Command '${command}' not found, check the docs and try again`);
     process.exit(1);
     break;
 }
