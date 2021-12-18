1081d5180de6efa85e052329ca19f98b0e79c8fe
David Burgos <david.burgos.risco@gmail.com>
2021-12-18 11:41:47 +0100

    Refactor to improve performance

--- display.js
+++ display.js
@@ -3,12 +3,18 @@ const core = require('./lib/core');
 
 async function run() {
   try {
+    const startTime = Date.now();
+    const [uploads, content] = await Promise.all([
+      ghost.uploads.list(),
+      ghost.content.fetch()
+    ]);
     const input = {
-      uploads: await ghost.uploads.list(),
-      content: await ghost.content.fetch()
+      uploads,
+      content
     };
     const unused = core.filterUnused(input);
     core.display(unused);
+    console.log(`⏱  Completed in ${(Date.now()-startTime)/1000}s`);
     return process.exit(0);
   } catch (err) {
     console.log(`❌ Error: ${err}`);
